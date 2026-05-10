d1dc06f9fb4fbb66d17d7fa7d5868b345035222e
Ruslan Bilohash <138953118+Ruslan-Bilohash@users.noreply.github.com>
2026-05-10 18:18:25 +0200

    Update send.php

--- en/send.php
+++ en/send.php
@@ -16,7 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] !== "POST") {
 }
 
 // === reCAPTCHA Protection ===
-$recaptcha_secret = '6LdpY-AsAAAAANw1Q0QrPAPyhqQwhJwkF8CwIiaB';
+$recaptcha_secret = '';
 $recaptcha_response = $_POST['g-recaptcha-response'] ?? '';
 
 if (empty($recaptcha_response)) {
@@ -70,7 +70,7 @@ try {
     $mail->Host       = 'smtp.hostinger.com';
     $mail->SMTPAuth   = true;
     $mail->Username   = 'email@golden-hands.lt';
-    $mail->Password   = 'Odifar78@';
+    $mail->Password   = '@';
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
     $mail->Port       = 587;
     $mail->CharSet    = 'UTF-8';
